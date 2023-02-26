9191bde13a436cf7fb82f9dbdc967b1b3da5322e
daheeisLegend <120437548+daheeisLegend@users.noreply.github.com>
2023-02-26 16:23:56 +0900

    230223 김다희 두더지 게임

--- app/src/main/java/com/kdh/app230223/Fragment1.java
+++ app/src/main/java/com/kdh/app230223/Fragment1.java
@@ -1,64 +1,181 @@
 package com.kdh.app230223;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.Message;
+import android.os.Vibrator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment1#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.w3c.dom.Text;
+
+import java.util.Random;
+
+
 public class Fragment1 extends Fragment {
+    int cnt;
+    ImageView [] moles = new ImageView[9];
+    MoleThread [] threads = new MoleThread[9];
+    TextView tvScore, tvHide;
+    TextView tvTime;
+    Button btnStart;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Fragment1() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment1.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Fragment1 newInstance(String param1, String param2) {
-        Fragment1 fragment = new Fragment1();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    Vibrator vibrator;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_1, container, false);
+        View view = inflater.inflate(R.layout.fragment_1, container, false);
+        tvScore = view.findViewById(R.id.tvScore);
+        tvTime = view.findViewById(R.id.tvTime);
+        vibrator = (Vibrator) getContext().getSystemService(Context.VIBRATOR_SERVICE);
+        tvHide = view.findViewById(R.id.tvHide);
+        btnStart = view.findViewById(R.id.btnStart);
+        // 두더지 9개 findViewById
+        btnStart.setOnClickListener(v->{
+            cnt = 0;
+            tvScore.setText("0");
+            btnStart.setVisibility(View.GONE);
+            tvHide.setVisibility(View.GONE);
+            new ThreadTime().start();
+            for(int i = 0; i< moles.length;i++){
+                int imgId = getResources().getIdentifier("imageView"+(i+1),"id",getContext().getPackageName());
+                // getContext() 액티비티가 아니라서 적어줌(activity 정보를 다 끌어옴)
+                moles[i] = view.findViewById(imgId);
+                threads[i] = new MoleThread(moles[i]);
+                threads[i].start();
+                moles[i].setTag("0");
+                moles[i].setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        if(v.getTag().toString().equals("1")){
+                            cnt++;
+                            Toast.makeText(getContext(), "잡았다 요놈", Toast.LENGTH_SHORT).show();
+                            v.setTag("0");
+                            tvScore.setText(cnt+"");
+
+                        }else{
+                            Toast.makeText(getContext(), "에이~ 아깝다!", Toast.LENGTH_SHORT).show();
+                            cnt--;
+                            tvScore.setText(cnt+"");
+                            vibrator.vibrate(200);
+                        }
+                    }
+                });
+            }
+        });
+
+
+        return view;
+    }
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            ((ImageView)msg.obj).setImageResource(msg.arg1);
+            ((ImageView)msg.obj).setTag(msg.arg2);
+        }
+    };
+
+    Handler timeHandler = new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            ((TextView)msg.obj).setText(msg.arg1+"");
+        }
+    };
+
+    Handler showScoreHandler = new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            ((TextView)msg.obj).setText("최종 점수 : "+cnt);
+            btnStart.setVisibility(View.VISIBLE);
+            btnStart.setText("다시하기");
+            tvHide.setVisibility(View.VISIBLE);
+
+
+        }
+    };
+
+
+
+    class MoleThread extends Thread{
+        private ImageView img;
+        public MoleThread(ImageView img){
+            this.img = img;
+        }
+
+        @Override
+        public void run() {
+
+            while(true){
+             // 랜덤시간만큼 내려간 상태 유지
+                int OffTime = new Random().nextInt(5000)+500;
+
+                try {
+                    Thread.sleep(OffTime);
+                    // 두더지가 올라가는 이미지로 바꿔줄 것임
+                    Message msg = new Message();
+                    msg.obj = img;
+                    msg.arg1 = R.drawable.mole3;
+                    msg.arg2 = 1;
+                    // 핸들러한테 보내줄 것임
+                    handler.sendMessage(msg);
+
+                    int onTime = new Random().nextInt(1000)+500;
+                    Thread.sleep(onTime);
+                    // 한번 보낸 Message 객체는 재활용 X
+                    Message msg2 = new Message();
+                    msg2.obj = img;
+                    msg2.arg1= R.drawable.mole2;
+                    msg2.arg2 = 0;
+                    handler.sendMessage(msg2);
+
+                } catch (InterruptedException e) {
+                    Message msg3 = new Message();
+                    msg3.obj = tvScore;
+                    showScoreHandler.sendMessage(msg3);
+                    break;
+                }
+
+
+            }
+        }
+    }
+
+    class ThreadTime extends Thread{
+        @Override
+        public void run() {
+            for(int i = 3;i>=0;i--){
+                Message msg = new Message();
+                try {
+                    Thread.sleep(1000);
+                    msg.obj = tvTime;
+                    msg.arg1 = i;
+                    timeHandler.sendMessage(msg);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            for(int i = 0; i<threads.length; i++){
+                threads[i].interrupt();
+                moles[i].setEnabled(false);
+            }
+
+        }
     }
 }
--- app/src/main/java/com/kdh/app230223/Fragment4.java
+++ app/src/main/java/com/kdh/app230223/Fragment4.java
@@ -1,19 +1,29 @@
 package com.kdh.app230223;
 
+import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.sql.Time;
 
 
 public class Fragment4 extends Fragment {
     Button btn1, btn2;
     TextView tv1, tv2;
+    int cnt1, cnt2;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,9 +36,87 @@ public class Fragment4 extends Fragment {
             tv1 = view.findViewById(R.id.tv1);
             tv2 = view.findViewById(R.id.tv2);
 
+
             // 1. 1~30까지 숫자를 세는 Thread 설계 (클래스로) -> 설계는 1개
             // 2. 버튼 눌렀을 때 Thread 객체를 생성하고 start -> 생성은 2개
+            btn1.setOnClickListener(v -> {
+                if(cnt1==0) {
+                    TimeThread thread = new TimeThread(tv1);
+                    thread.start();
+                    cnt1++;
+                }else{
+//                    Toast.makeText(, "누르지마!!!!", Toast.LENGTH_SHORT).show();
+                }
 
+            });
+            btn2.setOnClickListener(v -> {
+               if(cnt2==0) {
+                   TimeThread thread = new TimeThread(tv2);
+                   thread.start();
+                   cnt2++;
+               }else{
+                   Toast.makeText(getContext().getApplicationContext(), "그만 누르라고~~~", Toast.LENGTH_SHORT).show();
+               }
+            });
         return view;
     }
+
+
+
+    Handler handler = new Handler(){
+        // handler에 메세지 보낼 때는 sendMessage 메소드 사용
+        // 받은 메세지 처리할 때는 handleMessage 메소드 사용
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+                msg.toString();
+            // 매개변수 Message 객체는 Thread에서 보내준 메세지 그대로
+            // 받아온 메세지 객체에서 TextView랑 i값 꺼내서 setText 해보기
+            ((TextView)msg.obj).setText(String.valueOf(msg.arg1));
+
+        }
+    };
+
+
+    class TimeThread extends Thread{
+
+        // Thread 메소드 간추린 Life Cycle
+        // start -> run(커스텀 가능) -> destroy
+        private TextView tv;
+        public TimeThread(TextView tv){
+            this.tv= tv;
+        };
+
+        @Override
+        public void run() {
+
+            for(int i = 1; i<=30;i++){
+                //                tv.setText(i+"");
+
+                // 개발자가 설계한 Thread에서는 UI작업(setText, setImageResource etc..) 못함 <- 뒤죽박죽되니까 android에서 막음
+                // Handler에게 요청(message를 보냄)해서 MainThread로 작업을 전달!
+
+                Message msg = new Message();
+                // 예전에 배웠던 viewHolder랑 비슷(데이터를 담는 역할만 함!)
+                // Object타입 객체 1개(obj), int타입 변수 2개(arg1, arg2)를 담을 수 있음
+
+                // 이 예제에서 Handler한테 보내줘야할 데이터는?
+                // -> 숫자를 적어야하는 TextView, 적힐 숫자(i)
+                msg.obj = tv;
+                msg.arg1 = i;
+                handler.sendMessage(msg); // 위에서 구성한 msg 객체 보내기!
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+        }
+    }
+
+
+
 }
